cfd77788dec714416185f81fc07c2d22f33839b7
ming <ming@mingdeMacBook-Pro.local>
2019-12-05 13:56:54 +0800

    counter without redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Counter from './components/Counter'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Par extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            num: 0
+        }
+    }
+    handleIncrement() {
+        let tmpNum = this.state.num;
+        this.setState({
+            num: ++tmpNum
+        })
+    }
+    handleDecrement() {
+        let tmpNum = this.state.num;
+        this.setState({
+            num: --tmpNum
+        })
+    }
+    render() {
+        return (
+            <Counter
+                value={this.state.num}
+                onIncrement={() => this.handleIncrement()}
+                onDecrement={() => this.handleDecrement()}
+            />
+        );
+    }
+}
+
+ReactDOM.render(
+    <Par />,
+    document.getElementById('root')
+);
